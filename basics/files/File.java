705e66d336c0d4dc0b4b03cbb6bfde9319b83736
seham <104871821+sseham@users.noreply.github.com>
2022-05-10 11:51:03 -0400

    Create File.java

---
+++ basics/files/File.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class File {
+
+    public File(String file) {
+    }
+
+    public static Scanner scanS = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//
+        System.out.print("Enter the name of a file: ");
+        String fileName = scanS.nextLine(); 
+        
+        File myFile = new File("D:\\netbeansfiles\\" + fileName);
+//        Scanner readFile = new Scanner(myFile);
+//
+//        String line;
+//
+//        while (readFile.hasNextLine()) {
+//            line = readFile.nextLine();
+//            System.out.println(line);
+
+    }
+}
+
