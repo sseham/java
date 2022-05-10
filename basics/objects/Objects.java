734811a328340e120e7a51048668e32be0f5e46f
seham <104871821+sseham@users.noreply.github.com>
2022-05-10 12:00:22 -0400

    Create Objects.java

---
+++ basics/objects/Objects.java
@@ -0,0 +1,19 @@
+package objects;
+
+/**
+ *
+ * @author Sseha
+ */
+public class Objects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Employee lol = new Employee();
+        
+        System.out.println(lol.getName() + lol.calcWage());
+    }
+    
+}
