e323a3f37ee59a74e8d66b6805ed1d11a98bf3d0
seham <104871821+sseham@users.noreply.github.com>
2022-05-10 11:42:26 -0400

    Create Arrays.java

---
+++ basics/arrays/Arrays.java
@@ -0,0 +1,29 @@
+
+package arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+       int [] intA = new int[3];
+       String [] stringA = new String[5];
+       double[] doubleA = new double[4];
+       //int array
+       intA[0]=0;
+       intA[1]=5;
+       intA[2]=9;
+       //string array
+       stringA[0]="hello";
+       stringA[1]="goodbye";
+       stringA[2]="ciao";
+       stringA[3]="ola";
+       //ddbl array
+       doubleA[0]=0.0;
+       doubleA[1]=0.5;
+       doubleA[2]=0.6;
+       doubleA[3]=0.7;
+       //char array 0-0
+       
+              
+    }
+    
+}
