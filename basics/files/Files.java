29e0f0a1c96a8353bb11f78f33c6db843fb9e8e6
seham <104871821+sseham@users.noreply.github.com>
2022-05-10 11:51:47 -0400

    Create Files.java

---
+++ basics/files/Files.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package files;
+import java.io.*;
+import java.util.Scanner;
+public class Files {
+    public static Scanner scanS = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        
+        int sum = 0;
+       
+        System.out.print("Enter the name of a file: ");
+        String fileName = scanS.nextLine(); 
+        
+        File myFile = new File("D:\\netbeansfiles\\" + fileName);
+        Scanner readFile = new Scanner(myFile);
+
+        String line;
+
+        while (readFile.hasNextLine()) {
+            line = readFile.nextLine();
+            String[] token = line.split(", ");
+            System.out.println(token[0] + " has won $" + token[1] + "!");
+            sum += Integer.parseInt(token[1]);
+            String user = token[0];
+    }
+    System.out.println("The total winnings are $" + sum + "!");
+    
+}
+}
